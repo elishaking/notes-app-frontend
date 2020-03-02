d5f830b3379c15e075582b707bdd3f55f2e7dce6
King Elisha <ek.chibueze@gmail.com>
2020-03-02 20:08:40 +0100

    isAuthenticating

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 import Routes from "./Routes";
 import "./App.scss";
 
 function App() {
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
 
   function handleLogout() {
     userHasAuthenticated(false);
   }
 
-  return (
+  return isAuthenticating ? (
+    <h1>Authenticating...</h1>
+  ) : (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
